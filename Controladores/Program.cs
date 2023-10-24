457ebbbed85dc23b453b816c33282872799e3e50
DESKTOP-A37MTN2\csi22-jzarcia <jose.manuel.zarco.garcia@gmail.com>
2023-10-24 13:21:08 +0200

    no message

---
+++ Controladores/Program.cs
@@ -0,0 +1,27 @@
+﻿using Ejercicio2NumCuadrado.Servicios;
+
+namespace Ejercicio2NumCuadrado.Controladores
+{
+
+    class Program
+    {
+
+        static void Main(string[] args)
+        {
+            PedirNumeroInterfaz pn = new PedirNumeroImplementacion();
+            double numPedido,raiz;
+           
+
+            numPedido = pn.pedirNumero();
+            raiz = Math.Sqrt(numPedido);
+
+            if (raiz*raiz==numPedido)
+                Console.WriteLine("El numero es entero");
+            else
+                Console.WriteLine("El numero no es entero");
+
+        }
+
+    }
+
+}
---
+++ Servicios/PedirNumeroImplementacion.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ejercicio2NumCuadrado.Servicios
+{
+    internal class PedirNumeroImplementacion : PedirNumeroInterfaz
+    {
+
+        public double pedirNumero()
+        {
+            double numeroPedido;
+            Console.WriteLine("Escriba un nuemro entero ");
+            numeroPedido = Convert.ToDouble(Console.ReadLine()); 
+
+            return numeroPedido;
+        }
+
+    }
+}
